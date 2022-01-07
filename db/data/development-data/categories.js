b959adac472d5cc3626458c068ac189b16db7c9f
corkesw <home@stephencorke.com>
2022-01-07 17:44:55 +0000

    add sample category data

---
+++ db/data/development-data/categories.js
@@ -0,0 +1,8 @@
+module.exports = [
+  { category: "films" },
+  { category: "tv shows" },
+  { category: "restaurants" },
+  { category: "recipes" },
+];
+
+// "tv shows", "restaurants", "recipes"];
---
+++ db/data/development-data/index.js
@@ -0,0 +1,1 @@
+exports.categoryData = require("./categories.js");
